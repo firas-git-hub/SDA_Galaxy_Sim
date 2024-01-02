7d6f56ffbc4ffceb90aebecba448d4a874b99d56
firas <firas.moukhallalaty@gmail.com>
2024-01-02 04:52:36 +0100

    some edits, made sure the syntax is correct + added some comments. tested most of the insert implementation

--- src/Models/BHTreeNode.py
+++ src/Models/BHTreeNode.py
@@ -16,6 +16,13 @@ class BHTreeNode:
     # le facteur distance entre quadrant et particule / diametre du quadrant
     theta = None
     
+    # les particules qui ne sont pas assignees a un noeud de l'arbre
+    objetsNonAssignees = []
+    
+    # statistique pour voire combien de caluls on etaient fait pour estimer la force.
+    # Dans le code cpp c'est l'attribut s_stat._nNumCalc
+    nbCalculsPourEstimerForce = 0
+    
     def __init__(self, parent, min: Point, max: Point):
         
         self.quadrant = ''
@@ -24,42 +31,41 @@ class BHTreeNode:
         self.parent = parent
         self.min = min
         self.max = max
-        self.centre = Point(min.x + (max.x - min.x) / 2.0, min.y + (max.y - min.y) / 2.0)
+        self.centre = Point(min.x + (max.x - min.x) / 2.0, min.y + (max.y - min.y) / 2.0) # Centre du neoud
         self.masse = 0
         self.enfants: list[BHTreeNode] = [None, None, None, None]
         self.centreDeMasse = Point()
-        self.nbCalculsPourEstimerForce = 0 # statistique pour voire combien de caluls on etaient fait pour estimer la force
-        self.calcApproximeePossible = False # Si le calculs approximative des forces est possible
-        self.objetsNonAssignees = [] # les particules qui ne sont pas assignees a un noeud de l'arbre
+        self.calcForceApproxPossible = False # Si le calculs approximative des forces est possible. dans cpp c'est l'attribut _bSubdivided
 
-    def isRacine(self):
+    def isRacine(self)-> bool:
         return self.parent == None
     
-    def isFeuille(self):
+    def isFeuille(self)-> bool:
         # pas d'enfants => noeud est une feuille
         return (self.enfants[0] == None and
                self.enfants[1] == None and
                self.enfants[2] == None and
                self.enfants[3] == None)
 
-    # def tresProche() <---- do i need this ?
+    def tresProche(self)-> bool:
+        return self.calcForceApproxPossible
 
-    def nbObjetsNonAssignees(self):
-        return len(self.objetsNonAssignees)
-
-    def reiniStat(self):
-        if (not self.isRacine()):
-            return
-        self.nbCalculsPourEstimerForce = 0
-        self.__reiniDrapeau()
-
+    def nbObjetsNonAssignees()-> int|None:
+        return len(BHTreeNode.objetsNonAssignees)
+    
     def __reiniDrapeau(self):
-        self.calcApproximeePossible = False
+        self.calcForceApproxPossible = False
         for i in range(4):
             if(self.enfants[i]):
                 self.enfants[i].__reiniDrapeau()
         return
-    
+
+    def reiniStat(self):
+        if (not self.isRacine()):
+            return
+        BHTreeNode.nbCalculsPourEstimerForce = 0
+        self.__reiniDrapeau()
+
     def reiniArbre(self, min: Point, max: Point):
         if (not self.isRacine()):
             return
@@ -74,19 +80,21 @@ class BHTreeNode:
         self.masse = 0
         self.centreDeMasse = Point()
         self.nbParticules = 0
-        self.objetsNonAssignees = []
+        BHTreeNode.objetsNonAssignees = []
 
-    def rammeneQuadrant(self, x: float, y: float):
-        if(x <= self.center.x and y <= self.center.y):
+    def obtenirQuadrant(self, x: float, y: float)-> str|None:
+        if(x <= self.centre.x and y <= self.centre.y):
             return 'SW'
-        elif (x<= self.center.x and y >= self.center.y):
+        elif (x <= self.centre.x and y >= self.centre.y):
             return 'NW'
-        elif (x >= self.center.x and y >= self.center.y):
+        elif (x >= self.centre.x and y >= self.centre.y):
             return 'NE'
-        elif (x > self.max.x or y > self.max.y or x < self.min.x or y < self.min.y):
+        elif (x >= self.centre.x and y <= self.centre.y):
             return 'SE'
+        elif (x > self.max.x or y > self.max.y or x < self.min.x or y < self.min.y):
+            raise RuntimeError("Pas possible de determinee quadrant\n fonc: obtenirQuadrant")
         else:
-            return None
+            raise RuntimeError("Pas possible de determiner quadrant\n fonc: obtenirQuadrant")
         
     def creeNoeudQuad(self, quadrant: str):
         match  quadrant:
@@ -94,23 +102,25 @@ class BHTreeNode:
                 return BHTreeNode(self, self.min, self.centre)
             case 'NW':
                 return BHTreeNode(self,
-                                  Point(self.min.x, self.center.y),
-                                  Point(self.center.x, self.max.y))
+                                  Point(self.min.x, self.centre.y),
+                                  Point(self.centre.x, self.max.y))
             case 'NE':
                 return BHTreeNode(self, self.centre, self.max)
             case 'SE':
                 return BHTreeNode(self,
-                                  Point(self.center.x, self.min.y),
-                                  Point(self.max.x, self.center.y))
+                                  Point(self.centre.x, self.min.y),
+                                  Point(self.max.x, self.centre.y))
             case _:
-                return None
-
+                raise RuntimeError("Pas possible de deeterminer quadrant\n fonc: creeNoeudQuad")
 
     def calcDistributionMasse(self):
         # dans le cas s'il y a un particule dans le quadrant,
         # la masse du noeud = mase du particule et centre de mass 
         # du noeud = position de ce particule
         if self.nbParticules == 1 :
+            assert(self.particule.position)
+            assert(self.particule.vitesse)
+            assert(self.particule.acceleration)
             self.masse = self.particule.masse
             self.centreDeMasse = self.particule.position
             
@@ -120,6 +130,9 @@ class BHTreeNode:
             self.masse = 0
             self.centreDeMasse = Point(0, 0)
             
+            # La maniere que cette methode recursive est arrangee fait d'une
+            # facon que la masse vas etre additionne en debutant avec
+            # les feuills jusqu'a le haut de l'arbre.
             for i in range(4):
                 if(self.enfants[i]):
                     self.enfants[i].calcDistributionMasse()
@@ -133,6 +146,7 @@ class BHTreeNode:
         acc = Point(0, 0)
         if particule1 == particule2 :
             return None
+        
         x1, y1 = particule1.position.x, particule1.position.y
         x2, y2, m2 = particule2.position.x, particule2.position.y, particule2.masse
         
@@ -156,33 +170,33 @@ class BHTreeNode:
         
         if self.nbParticules == 1:
             acc = BHTreeNode.calcAcc(particule1, self.particule)
-            self.nbCalculsPourEstimerForce += 1
+            BHTreeNode.nbCalculsPourEstimerForce += 1
         
         else:
             r = np.sqrt((particule1.position.x - self.centreDeMasse.x) * (particule1.position.x - self.centreDeMasse.x) +
                  (particule1.position.y - self.centreDeMasse.y) * (particule1.position.y - self.centreDeMasse.y))
             d = self.max.x - self.min.x
-            if((d / r) <= self.theta):
-                self.calcApproximeePossible = False
-                k = self.s_gamma * self.masse / (r * r * r)
+            if((d / r) <= BHTreeNode.theta):
+                self.calcForceApproxPossible = False
+                k = BHTreeNode.s_gamma * self.masse / (r * r * r)
                 acc.x = k * (self.centreDeMasse.x - particule1.position.x)
                 acc.y = k * (self.centreDeMasse.y - particule1.position.y)
-                self.nbCalculsPourEstimerForce += 1
+                BHTreeNode.nbCalculsPourEstimerForce += 1
             else:
-                self.calcApproximeePossible = True
+                self.calcForceApproxPossible = True
                 buf: Point = Point(0, 0)
                 for q in range(4):
                     if self.enfants[q]:
-                        buf = self.enfants[q].calcForceArbre()
+                        buf = self.enfants[q].calcForceArbre(particule1)
                         acc.x += buf.x
                         acc.y += buf.y
         return acc
         
     def calcForce(self, particule: Particule)-> Point :
         acc: Point = self.calcForceArbre(particule)
-        if len(self.objetsNonAssignees):
-            for i in range(len(self.objetsNonAssignees)):
-                buf = BHTreeNode.calcAcc(particule, self.objetsNonAssignees[i])
+        if len(BHTreeNode.objetsNonAssignees):
+            for i in range(len(BHTreeNode.objetsNonAssignees)):
+                buf = BHTreeNode.calcAcc(particule, BHTreeNode.objetsNonAssignees[i])
                 acc.x += buf.x
                 acc.y += buf.y
         return acc
@@ -200,7 +214,75 @@ class BHTreeNode:
         for i in range(4):
             if self.enfants[i]:
                 self.enfants[i].imprimerNoeud(i, niveau+1)
+
+    # Cette fonction existe pour faire le lien entre l'enfant et le quadrant.
+    # Dans le code c++, cette fonctionalite est traduite par les nombre qu'un enum 
+    # assigne a chaque element. Par ex, dans l'enum de cPP NE prendra la valeur0
+    # NW prendra la valeur 1, etc. comme il sont l'element 1 et 2 du enum respectivement
+    def traduireQuadrantAIndexEnfant(quadrant):
+        match  quadrant:
+            case 'NE':
+                return 0
+            case 'NW':
+                return 1
+            case 'SW':
+                return 2
+            case 'SE':
+                return 3
+            case _:
+                return None
+
+    def insert(self, nouveauParticule: Particule, level):
+        p1: Particule = nouveauParticule
+
+        if((p1.position.x < self.min.x or p1.position.x > self.max.x) or (p1.position.y < self.min.y or p1.position.y > self.max.y)):
+            error_message = (
+            f"Particle position ({p1.position.x}, {p1.position.y}) "
+            f"is outside tree node (min.x={self.min.x}, max.x={self.max.x}, "
+            f"min.y={self.min.y}, max.y={self.max.y})"
+            )
+            raise RuntimeError(error_message)
         
+        # s'il y a plusieurs particules dans un noeud, donc il faut
+        # que ce noeud ait des enfants afin que le nb de partocules est egal a 1.
+        # s'il a plusieurs enfants, on vas le decoupe.
+        if self.nbParticules > 1:
+            eQuad = self.obtenirQuadrant(p1.position.x, p1.position.y)
+            eQuadIndex = BHTreeNode.traduireQuadrantAIndexEnfant(eQuad)
+            
+            if not self.enfants[eQuadIndex]:
+                self.enfants[eQuadIndex] = self.creeNoeudQuad(eQuad)
+
+            self.enfants[eQuadIndex].insert(nouveauParticule, level + 1)
+        
+        # si le nb de particules est 1, c'est sur que le neoud est une feuille,
+        # je cree des enfants pour ce noeud et je donne le particule premier
+        # a un des enfants.
+        elif self.nbParticules == 1:
+            assert(self.isFeuille() or self.isRacine())
+            p2 = self.particule
+
+            if (p1.position.x == p2.position.x) and (p1.position.y == p2.position.y):
+                 BHTreeNode.objetsNonAssignees.append(nouveauParticule)
+                 
+            else:
+                eQuad = self.obtenirQuadrant(p2.position.x, p2.position.y)
+                eQuadIndex = BHTreeNode.traduireQuadrantAIndexEnfant(eQuad)
+                if not self.enfants[eQuadIndex]:
+                    self.enfants[eQuadIndex] = self.creeNoeudQuad(eQuad)
+                self.enfants[eQuadIndex].insert(self.particule, level + 1)
+                self.particule = None
+
+                eQuad = self.obtenirQuadrant(p1.position.x, p1.position.y)
+                eQuadIndex = BHTreeNode.traduireQuadrantAIndexEnfant(eQuad)
+                if not self.enfants[eQuadIndex]:
+                    self.enfants[eQuadIndex] = self.creeNoeudQuad(eQuad)
+                self.enfants[eQuadIndex].insert(nouveauParticule, level + 1)
+        elif self.nbParticules == 0:
+            self.particule = nouveauParticule
+
+        self.nbParticules += 1
+     
 # THIS IS FOR TESTING
 
 
@@ -210,12 +292,27 @@ class BHTreeNode:
 # testParent.nbCalculsPourEstimerForce = 10
 # testChild1.nbCalculsPourEstimerForce = 15
 # testChild2.nbCalculsPourEstimerForce = 17
-# testChild1.calcApproximeePossible = True
-# testChild2.calcApproximeePossible = True
+# testChild1.calcForceApproxPossible = True
+# testChild2.calcForceApproxPossible = True
 # testParent.enfants[0] = testChild1
 # testParent.enfants[3] = testChild2
-# print(testParent.enfants[0].calcApproximeePossible)
-# print(testParent.enfants[3].calcApproximeePossible)
+# print(testParent.enfants[0].calcForceApproxPossible)
+# print(testParent.enfants[3].calcForceApproxPossible)
 # testParent.reiniStat()
-# print(testChild1.calcApproximeePossible)
-# print(testChild2.calcApproximeePossible)
+# print(testChild1.calcForceApproxPossible)
+# print(testChild2.calcForceApproxPossible)
+
+rootNode = BHTreeNode(None, Point(0,0), Point(20,20))
+p1 = Particule(Point(4,9), Point(1,1), Point(1,1), 250)
+p2 = Particule(Point(4,11), Point(1,1), Point(1,1), 250)
+p3 = Particule(Point(11,9), Point(1,1), Point(1,1), 250)
+p4 = Particule(Point(11,11), Point(1,1), Point(1,1), 250)
+p5 = Particule(Point(4,8), Point(1,1), Point(1,1), 250)
+rootNode.insert(p1,0)
+rootNode.insert(p2,0)
+rootNode.insert(p3,0)
+rootNode.insert(p4,0)
+rootNode.insert(p5,0)
+rootNode.insert(p5,0)
+
+print(rootNode.objetsNonAssignees)
--- src/Models/Point.py
+++ src/Models/Point.py
@@ -1,5 +1,11 @@
 class Point:
 
-    def __init__(self, x, y):
+    def __init__(self, x: int = None, y: int = None):
         self.x = x
         self.y = y
+        
+    def __str__(self):
+        return str("x: " 
+                   + str(self.x)
+                   + ", y: "
+                   + str(self.y))
