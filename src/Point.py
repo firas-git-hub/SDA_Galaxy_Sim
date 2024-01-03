60ef3eecde195f669ffd53d35c678b50a90dba84
Soroush HAGHSHENAS <soroush.haghshenas@edu.univ-paris13.fr>
2024-01-04 00:23:20 +0100

    particule_point_bhtreeNode

--- src/Point.py
+++ src/Point.py
@@ -1,10 +1,11 @@
 class Point:
-    def __init__(self, _x=0, _y=0):
-        self.x = _x
-        self.y = _y
 
-class Vec3D:
-    def __init__(self, _x=0, _y=0, _z=0):
-        self.x = _x
-        self.y = _y
-        self.z = _z
+    def __init__(self, x: float = None, y: float = None):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return str("x: "
+                   + str(self.x)
+                   + ", y: "
+                   + str(self.y))
